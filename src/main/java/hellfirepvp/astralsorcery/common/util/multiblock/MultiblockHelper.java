95ba04b0bfb35e1c9509aebc6a43a8fd6ed1192e
HellFirePvP <dendorfer.maximilian0@googlemail.com>
2016-10-19 17:38:14 +0200

    Manual additions
    Add missing json files and things manually.

---
+++ src/main/java/hellfirepvp/astralsorcery/common/util/multiblock/MultiblockHelper.java
@@ -0,0 +1,14 @@
+package hellfirepvp.astralsorcery.common.util.multiblock;
+
+/**
+ * This class is part of the Astral Sorcery Mod
+ * The complete source code for this mod can be found on github.
+ * Class: MultiblockHelper
+ * Created by HellFirePvP
+ * Date: 15.05.2016 / 15:40
+ */
+public class MultiblockHelper {
+
+
+
+}
