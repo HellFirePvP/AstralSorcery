b1d2fddfc98f4d79847c0507f565845ae72ab7c7
HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>
2019-04-19 18:25:03 +0200

    Add base class, Change build metadata directory

---
+++ src/main/java/hellfirepvp/astralsorcery/AstralSorcery.java
@@ -0,0 +1,21 @@
+package hellfirepvp.astralsorcery;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * This class is part of the Astral Sorcery Mod
+ * The complete source code for this mod can be found on github.
+ * Class: AstralSorcery
+ * Created by HellFirePvP
+ * Date: 19.04.2019 / 18:14
+ */
+@Mod(AstralSorcery.MODID)
+public class AstralSorcery {
+
+    public static final String MODID = "astralsorcery";
+    public static final String NAME = "Astral Sorcery";
+    public static final String VERSION = "1.11.0";
+    public static final String CLIENT_PROXY = "hellfirepvp.astralsorcery.client.ClientProxy";
+    public static final String COMMON_PROXY = "hellfirepvp.astralsorcery.common.CommonProxy";
+
+}
