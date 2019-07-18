9849dc9e870abd386c0785da065a2c64e5038f25
HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>
2019-07-18 10:59:05 +0200

    Add replaceable state configurations to aquamarine and marble generation

--- src/main/java/hellfirepvp/astralsorcery/common/world/attributes/GenAttributeAquamarine.java
+++ src/main/java/hellfirepvp/astralsorcery/common/world/attributes/GenAttributeAquamarine.java
@@ -8,9 +8,13 @@
 
 package hellfirepvp.astralsorcery.common.world.attributes;
 
+import com.google.common.collect.Lists;
+import hellfirepvp.astralsorcery.AstralSorcery;
 import hellfirepvp.astralsorcery.common.block.BlockCustomSandOre;
 import hellfirepvp.astralsorcery.common.data.config.Config;
+import hellfirepvp.astralsorcery.common.data.config.entry.ConfigEntry;
 import hellfirepvp.astralsorcery.common.lib.BlocksAS;
+import hellfirepvp.astralsorcery.common.util.MiscUtils;
 import hellfirepvp.astralsorcery.common.world.WorldGenAttribute;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
@@ -18,10 +22,14 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * This class is part of the Astral Sorcery Mod
@@ -32,19 +40,64 @@ import java.util.Random;
  */
 public class GenAttributeAquamarine extends WorldGenAttribute {
 
+    private List<IBlockState> replaceableStates = null;
+    private List<String> replaceableStatesSerialized = new ArrayList<>(); //Delay resolving states to a later state...
+
     public GenAttributeAquamarine() {
         super(0);
+        Config.addDynamicEntry(new ConfigEntry(ConfigEntry.Section.WORLDGEN, "aquamarine") {
+            @Override
+            public void loadFromConfig(Configuration cfg) {
+                String[] applicableReplacements = cfg.getStringList("ReplacementStates", getConfigurationSection(), new String[] {
+                        "minecraft:sand:0"
+                }, "Defines the blockstates that may be replaced by aquamarine shale when trying to generate aquamarine shale. format: <modid>:<name>:<meta> - Use meta -1 for wildcard");
+                replaceableStatesSerialized = Arrays.asList(applicableReplacements);
+            }
+        });
+    }
+
+    private void resolveReplaceableStates() {
+        replaceableStates = new LinkedList<>();
+        for (String stateStr : replaceableStatesSerialized) {
+            String[] spl = stateStr.split(":");
+            if(spl.length != 3) {
+                AstralSorcery.log.info("Skipping invalid replacement state: " + stateStr);
+                continue;
+            }
+            String strMeta = spl[2];
+            Integer meta;
+            try {
+                meta = Integer.parseInt(strMeta);
+            } catch (NumberFormatException exc) {
+                AstralSorcery.log.error("Skipping invalid replacement state: " + stateStr + " - Its 'meta' is not a number!");
+                continue;
+            }
+            Block b = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(spl[0], spl[1]));
+            if(b == null || b == Blocks.AIR) {
+                AstralSorcery.log.error("Skipping invalid replacement state: " + stateStr + " - The block does not exist!");
+                continue;
+            }
+            if(meta == -1) {
+                replaceableStates.addAll(b.getBlockState().getValidStates());
+            } else {
+                replaceableStates.add(b.getStateFromMeta(meta));
+            }
+        }
     }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world) {
+        if(replaceableStates == null) {
+            resolveReplaceableStates();
+        }
+
         for (int i = 0; i < Config.aquamarineAmount; i++) {
             int rX = (chunkX  * 16) + random.nextInt(16) + 8;
             int rY = 48 + random.nextInt(19);
             int rZ = (chunkZ  * 16) + random.nextInt(16) + 8;
             BlockPos pos = new BlockPos(rX, rY, rZ);
             IBlockState stateAt = world.getBlockState(pos);
-            if(!stateAt.getBlock().equals(Blocks.SAND)) {
+            if (MiscUtils.getMatchingState(this.replaceableStates, stateAt) == null) {
                 continue;
             }
 
