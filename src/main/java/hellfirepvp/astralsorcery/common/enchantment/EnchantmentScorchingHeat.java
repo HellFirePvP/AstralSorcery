f38f55ee548219e2cad0e6d662da45a6356e77fd
Shiny <ShinyTourettesX@gmail.com>
2021-06-04 04:07:12 +0800

    Allow books to apply the enchant via anvil. Fixes #1820

--- src/main/java/hellfirepvp/astralsorcery/common/enchantment/EnchantmentScorchingHeat.java
+++ src/main/java/hellfirepvp/astralsorcery/common/enchantment/EnchantmentScorchingHeat.java
@@ -33,6 +33,11 @@ public class EnchantmentScorchingHeat extends Enchantment {
     }
 
     @Override
+    public boolean canApply(ItemStack stack) {
+        return super.canApplyAtEnchantingTable(stack);
+    }
+
+    @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack) {
         return false;
     }
