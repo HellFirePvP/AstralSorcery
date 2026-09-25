c0a0b11878ee457bc46fbbaf9c0dbb6d5f2e9a06
HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>
2026-09-25 17:19:50 +0200

    Fix off-thread data manipulation during worldgen
    Signed-off-by: HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>

--- src/main/java/hellfirepvp/astralsorcery/common/worldgen/structure/processor/FocalPointRegisterProcessor.java
+++ src/main/java/hellfirepvp/astralsorcery/common/worldgen/structure/processor/FocalPointRegisterProcessor.java
@@ -19,6 +19,7 @@ import hellfirepvp.astralsorcery.common.util.data.ColumnPos;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistryCodecs;
+import net.minecraft.server.TickTask;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
@@ -63,7 +64,10 @@ public class FocalPointRegisterProcessor extends StructureProcessor {
                 this.getAvailableConstellations().getRandomElement(settings.getRandom(offset)).ifPresent(cstHolder -> {
                     // Only load then node if it's a server world, i.e. not during chunk generation
                     // The node will get loaded when the chunk is loaded, this processor runs pre-chunk loading
-                    FocalPointManager.getInstance().addNewNode(actualLevel, new BasicFocalPointNode(ColumnPos.of(offset), cstHolder.value()), levelGen instanceof ServerLevel);
+                    levelGen.getLevel().getServer().tell(new TickTask(0, () -> {
+                        //delay and resync to main thread
+                        FocalPointManager.getInstance().addNewNode(actualLevel, new BasicFocalPointNode(ColumnPos.of(offset), cstHolder.value()), levelGen instanceof ServerLevel);
+                    }));
                 });
             }
         }
