74d0ab8eb3acf8ac2330ee58e86e2ab5008236ee
HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>
2026-09-25 23:23:03 +0200

    Fix lumen crystallizer sometimes not accepting items again
    Signed-off-by: HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>

--- src/main/java/hellfirepvp/astralsorcery/common/tile/TileLumenCrystallizer.java
+++ src/main/java/hellfirepvp/astralsorcery/common/tile/TileLumenCrystallizer.java
@@ -345,9 +345,9 @@ public class TileLumenCrystallizer extends TileEntityTick<TileLumenCrystallizer.
                     .extractFilter((slot, amount, existing) -> false)
                     .inputFilter((slot, toAdd, existing) -> {
                         if (!existing.isEmpty()) return false;
-                        return this.findMatchingRecipe(toAdd).isPresent() &&
-                                (this.getContainedLumen().isEmpty() ||
-                                        this.findMatchingRecipe(this.getContainedLumen().getLumen()).equals(this.findMatchingRecipe(toAdd)));
+                        var match = this.findMatchingRecipe(toAdd);
+                        return match.isPresent() &&
+                                (this.getContainedLumen().isEmpty() || match.get().value().getLumenToCrystallize().equals(this.getContainedLumen().getLumen()));
                     })
                     .accessibleSides(Direction.DOWN);
         }
